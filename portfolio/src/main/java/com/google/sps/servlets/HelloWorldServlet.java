4feb5f7b3dda0d19f866552111a4178a52384491
JuliaSull <JulianSull@aol.com>
2021-03-05 09:46:22 +0000

    Completes the JSON portion of the tutorial.

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,10 +1,14 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
@@ -13,6 +17,24 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+
+    ArrayList<String> values = new ArrayList<>();
+    values.add("Zero");
+    values.add("One");
+    values.add("Two");
+
+
+
+    response.getWriter().println(convertToJsonUsingGson(values));
+  }
+
+  /**
+   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
+   * the Gson library dependency to pom.xml.
+   */
+  private String convertToJsonUsingGson(ArrayList<String> arrOfStr) {
+    Gson gson = new Gson();
+    String json = gson.toJson(arrOfStr);
+    return json;
   }
 }
